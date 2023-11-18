e0b659f788844a1ae1b399056b1805a4b8a9061f
Bob Jiang <78698927+Jiang-Feiyu@users.noreply.github.com>
2023-11-18 12:59:40 +0800

    Update bluetooth.py

--- Python/bluetooth.py
+++ Python/bluetooth.py
@@ -1,22 +1,48 @@
 import serial
 import time
 
-# 蓝牙串口虚拟文件
-port = '/dev/rfcomm0'
+def connect_arduino(port, baudrate):
+    try:
+        # 打开串口
+        ser = serial.Serial(port, baudrate, timeout=1)
+        print(f"成功连接到Arduino，端口：{ser.name}, 波特率：{baudrate}")
+        return ser
+    except serial.SerialException as e:
+        print(f"连接失败：{e}")
+        return None
 
-# 设置蓝牙波特率
-bluetooth_serial = serial.Serial(port, 9600)
+def send_command(ser, command):
+    try:
+        # 发送指令
+        ser.write(command.encode())
+        print(f"已发送指令: {command}")
+    except serial.SerialException as e:
+        print(f"发送指令失败：{e}")
 
-# 多次发送保证发送成功
-for i in range(1, 10):
-    # 要发送的数据
-    data_to_send = f"Hello, message {i}"
+def main():
+    # 蓝牙串口虚拟文件
+    port = '/dev/rfcomm0'
 
-    # 将字符串转换为字节对象并发送
-    bluetooth_serial.write(data_to_send.encode('utf-8'))
+    # 设置蓝牙波特率
+    bluetooth_serial = serial.Serial(port, 9600)
+    
+    # 请替换成你的软串口端口和波特率
+    baud_rate = 9600  # 根据实际情况修改
 
-    # 等待一段时间，确保数据被发送
-    time.sleep(1)
+    # 连接到Arduino
+    arduino_serial = connect_arduino(port, baud_rate)
 
-# 关闭蓝牙串口连接
-bluetooth_serial.close()
+    if arduino_serial is not None:
+        try:
+            # 向Arduino发送指令
+            send_command(arduino_serial, "This is python")
+
+            # 这里可以添加其他操作
+
+        finally:
+            # 关闭串口连接
+            arduino_serial.close()
+            print("已关闭串口连接")
+
+if __name__ == "__main__":
+    main()
