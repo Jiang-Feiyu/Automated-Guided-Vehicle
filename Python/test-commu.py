b33d3518ae4ac86b5abb6c41522f200159ea3722
Brian-Chen-0706 <78289649+Brian-Chen-0706@users.noreply.github.com>
2023-11-20 17:53:44 +0800

    Update test-commu.py

--- Python/test-commu.py
+++ Python/test-commu.py
@@ -12,24 +12,33 @@ def find_arduino_port():
 
 # 查找Arduino串口号
 arduino_port = find_arduino_port()
+print("arduino_port:", arduino_port)
 
 if arduino_port:
     # 设置串口参数
     ser = serial.Serial(arduino_port, 9600, timeout=1)
+    print("Serial name:", ser.name)
+    print("Serial baudrate:", ser.baudrate)
+    print("Serial state:", ser.is_open)
 
-    # 向Arduino发送消息
-    ser.write("This is Python\r\n".encode('utf-8'))
+    while 1:
 
-    # 等待一段时间，以确保Arduino有足够的时间处理消息并发送回复
-    time.sleep(5)
+        res=ser.readline()
+        print(res)
+        
+        # 向Arduino发送消息
+        ser.write("This is Python\r\n".encode('utf-8'))
 
-    # 读取Arduino的回复
-    response = ser.readline().decode('utf-8').strip()
+        # 等待一段时间，以确保Arduino有足够的时间处理消息并发送回复
+        time.sleep(5)
 
-    # 打印回复消息
-    print("Arduino says:", response)
+        # 读取Arduino的回复
+        response = ser.readline().decode('utf-8').strip()
 
-    # 关闭串口连接
-    ser.close()
+        # 打印回复消息
+        print("Arduino says:", response)
+
+        # 关闭串口连接
+        ser.close()
 else:
     print("Arduino not found.")
