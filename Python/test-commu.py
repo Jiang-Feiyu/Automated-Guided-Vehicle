d9043936fff6ed9170bb7016ff88ee4d9e24df8e
Bob Jiang <78698927+Jiang-Feiyu@users.noreply.github.com>
2023-11-20 16:08:48 +0800

    Update test-commu.py

--- Python/test-commu.py
+++ Python/test-commu.py
@@ -1,7 +1,22 @@
+# Python Code
+
 import serial
+import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600) # 串口设备和波特率要和Arduino一致
+# 设置串口参数
+ser = serial.Serial('COM3', 9600, timeout=1)
 
-while True:
-    data = ser.readline().decode('utf-8').strip()
-    print(f"Received data: {data}")
+# 向Arduino发送消息
+ser.write("This is Python\n".encode('utf-8'))
+
+# 等待一段时间，以确保Arduino有足够的时间处理消息并发送回复
+time.sleep(2)
+
+# 读取Arduino的回复
+response = ser.readline().decode('utf-8').strip()
+
+# 打印回复消息
+print("Arduino says:", response)
+
+# 关闭串口连接
+ser.close()
