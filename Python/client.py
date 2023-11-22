c52407233540cfc284f7a26ea95f29e4b2acdf92
Bob Jiang <78698927+Jiang-Feiyu@users.noreply.github.com>
2023-11-22 14:12:09 +0800

    Update client.py

--- Python/client.py
+++ Python/client.py
@@ -1,25 +1,28 @@
 import requests
 import time
 
+def send_message_to_server(server_address, message):
+    # 发送HTTP POST请求，将消息发送给服务器
+    response_post = requests.post(server_address, data=message)
+
+    # 打印服务器的响应
+    print("Server Response:")
+    print(response_post.text)
+
 def listen_to_server(server_address):
     while True:
+        # 获取用户输入的消息
+        message_to_server = input("Enter your message to the server: ")
+
+        # 发送HTTP POST请求，将用户输入的消息发送给服务器
+        send_message_to_server(server_address, message_to_server)
+
         # 发送HTTP GET请求
         response = requests.get(server_address)
 
         # 打印服务器响应
         print("Server Response:")
         print(response.text)
-
-        # 回应服务器
-        response_to_server = "Received"
-        print("Sending response to server:", response_to_server)
-
-        # 发送HTTP POST请求，将回应发送给服务器
-        response_post = requests.post(server_address, data=response_to_server)
-
-        # 打印服务器的响应
-        print("Server Response:")
-        print(response_post.text)
 
         # 休眠一段时间，可以根据实际情况调整
         time.sleep(5)
